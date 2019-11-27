c14dd93bb399534fcccad4e1b71952408b65360a
Felipe <fpr29.10@gmail.com>
2019-11-27 14:23:03 -0600

    provide firebase in react

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
@@ -15,13 +15,15 @@ const App = () => (
 	<Router>
 		<Navigation />
 		<hr />
-		<Route exact path={ROUTES.LANDING} component={LandingPage} />
-		<Route path={ROUTES.SIGN_IN} component={SignInPage} />
-		<Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-		<Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-		<Route path={ROUTES.HOME} component={HomePage} />
-		<Route path={ROUTES.ACCOUNT} component={AccountPage} />
-		<Route path={ROUTES.ADMIN} component={AdminPage} />
+		<Switch>
+			<Route exact path={ROUTES.LANDING} component={LandingPage} />
+			<Route path={ROUTES.SIGN_IN} component={SignInPage} />
+			<Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+			<Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+			<Route path={ROUTES.HOME} component={HomePage} />
+			<Route path={ROUTES.ACCOUNT} component={AccountPage} />
+			<Route path={ROUTES.ADMIN} component={AdminPage} />
+		</Switch>
 	</Router>
 );
 export default App;
