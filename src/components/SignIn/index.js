973467c8b5990fd572562b01bc772aa06bbdee1d
Felipe Parra <fpr29.10@gmail.com>
2019-11-28 16:52:37 -0600

    sign in firebase

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,6 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
-const SignIn = () => {
-	return <div>SignIn</div>;
+import { SignUpLink } from '../SignUp';
+import { withFirebase } from '../Firebase';
+
+import * as ROUTES from '../../constants/routes';
+
+const SignInPage = () => {
+	return (
+		<div className="container p-4">
+			<h1>Sign In</h1>
+			<SignInForm />
+			<SignUpLink />
+		</div>
+	);
 };
-export default SignIn;
+
+const INITIAL_STATE = {
+	email: '',
+	password: '',
+	error: null
+};
+class SignInFormBase extends Component {
+	state = { ...INITIAL_STATE };
+	onSubmit = (event) => {
+		const { email, password } = this.state;
+		this.props.firebase
+			.doSignInWithEmailAndPassword(email, password)
+			.then(() => {
+				this.setState({ ...INITIAL_STATE });
+				this.props.history.push(ROUTES.HOME);
+			})
+			.catch((error) => {
+				this.setState({ error });
+			});
+		event.preventDefault();
+	};
+
+	onChange = (event) => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+	render() {
+		const { email, password, error } = this.state;
+		const isInValid = password === '' || email === '';
+		return (
+			<form onSubmit={this.onSubmit}>
+				<div className="form-row">
+					<div className="form-group col-md-6">
+						<input
+							className="form-control"
+							name="email"
+							value={email}
+							onChange={this.onChange}
+							type="email"
+							placeholder="Email Address"
+						/>
+					</div>
+					<div className="form-group col-md-6">
+						<input
+							className="form-control"
+							name="password"
+							value={password}
+							onChange={this.onChange}
+							type="password"
+							placeholder="Password"
+						/>
+					</div>
+				</div>
+				<button className="btn btn-primary btn-block" disabled={isInValid} type="submit">
+					Sign In
+				</button>
+				{error && <p className="text-danger">{error.message}</p>}
+			</form>
+		);
+	}
+}
+
+const SignInForm = compose(withRouter, withFirebase)(SignInFormBase);
+
+export default SignInPage;
+
+export { SignInForm };
