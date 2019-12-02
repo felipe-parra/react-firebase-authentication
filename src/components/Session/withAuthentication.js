313cc6b823d06b985f31861447a092feb5b1774c
Felipe Parra <fpr29.10@gmail.com>
2019-12-02 01:14:09 -0600

    session persistence

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -6,14 +6,16 @@ import { withFirebase } from '../Firebase';
 const withAuthentication = (Component) => {
 	class WithAuthentication extends React.Component {
 		state = {
-			authUser: null
+			authUser: JSON.parse(localStorage.getItem('authUser'))
 		};
 		componentDidMount() {
 			this.listener = this.props.firebase.onAuthUserListener(
 				(authUser) => {
+					localStorage.setItem('authUser', JSON.stringify(authUser));
 					this.setState({ authUser });
 				},
 				() => {
+					localStorage.removeItem('authUser');
 					this.setState({ authUser: null });
 				}
 			);
